0556730523bc7d52132bf9fe0351d3d0c5dc6090
STRANOstudios <friand2021@gmail.com>
2025-12-04 15:48:30 +0100

    update

---
+++ Source/TheGauntlet2/Game/Components/Requirements/Toggle_Req.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Game/Components/Requirements/RequirementComponent.h"
+#include "Toggle_Req.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THEGAUNTLET2_API UToggle_Req : public URequirementComponent
+{
+	GENERATED_BODY()
+	
+};
